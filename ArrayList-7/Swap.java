bd6b22b18087f1fd81f88f98d7b6fcf498227f19
Poonam Gaikwad <gaikwadpoonam005@gmail.com>
2026-07-23 00:45:09 +0530

    Initial Commit

---
+++ ArrayList-7/Swap.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Swap{
+
+    public static void Swapping(ArrayList<Integer> list, int idx1, int idx2){
+
+        int temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);
+
+
+    }
+public  static void main(String[] arggs){
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(3);
+    list.add(6);
+    list.add(8);
+    list.add(5);
+
+    System.out.print(list);
+    Swapping(list, 2, 0);
+    System.out.print(list);
+  
+}
+}
